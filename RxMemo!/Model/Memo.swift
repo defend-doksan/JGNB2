f9f749c2e6151db8c2743c6e8f67a53c43c88222
sungchul kim <sheep5568@gmail.com>
2021-05-29 22:55:48 +0900

    add memo model

---
+++ RxMemo!/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  RxMemo!
+//
+//  Created by 김성철 on 2021/05/29.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+         
+}
