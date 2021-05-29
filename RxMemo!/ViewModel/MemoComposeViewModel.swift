fcb0fb4c26d40c25d0f9c301273069343b4201d4
sungchul kim <sheep5568@gmail.com>
2021-05-29 23:34:34 +0900

    add view model files

---
+++ RxMemo!/ViewModel/MemoComposeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoComposeViewModel.swift
+//  RxMemo!
+//
+//  Created by 김성철 on 2021/05/29.
+//
+
+import Foundation
+
+class MemoComposeViewModel {
+    
+}
---
+++ RxMemo!/ViewModel/MemoDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoDetailViewModel.swift
+//  RxMemo!
+//
+//  Created by 김성철 on 2021/05/29.
+//
+
+import Foundation
+
+class MemoDetailViewModel {
+    
+}
