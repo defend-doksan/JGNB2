640f4c9ab7860c0fc75cd7d8f9a9983dccaf8e75
sungchul kim <sheep5568@gmail.com>
2021-06-07 17:59:13 +0900

    memo delete

--- RxMemo!/ViewModel/MemoListViewModel.swift
+++ RxMemo!/ViewModel/MemoListViewModel.swift
@@ -49,5 +49,10 @@ class MemoListViewModel: CommonViewModel {
         }
     }()
     
+    lazy var deleteAction: Action< Memo, Swift.Never> = {
+        return Action { memo in
+            return self.storage.delete(memo: memo).asObservable().ignoreElements()
+        }
+    }()
    
 }
